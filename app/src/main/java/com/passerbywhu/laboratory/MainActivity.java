0a8d91fdeeadbbb82db7216c83ba4740904ffd5e
hzwuwenchao <hzwuwenchao@corp.netease.com>
2018-01-05 12:04:14 +0800

    提前RecyclerView点击事件测试

---
+++ app/src/main/java/com/passerbywhu/laboratory/MainActivity.java
@@ -0,0 +1,26 @@
+package com.passerbywhu.laboratory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //事实证明。RecyclerView不会响应onClick事件。只能用onTouch去处理。
+                int id = v.getId();
+            }
+        });
+    }
+}
